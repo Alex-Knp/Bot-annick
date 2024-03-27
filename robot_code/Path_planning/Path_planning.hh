c9f0ef38352cf754e7b473d746a606262dfd2393
trebelge <romain.englebert@student.uclouvain.be>
2024-03-27 17:19:50 +0100

    a

--- robot_code/Path_planning/Path_planning.hh
+++ robot_code/Path_planning/Path_planning.hh
@@ -15,4 +15,12 @@
 // Variables déclaration 
 //double 
 
+typedef struct Path_planning
+{
+	double norm;  ///< current norm of the force vector field
+	double theta; ///< current angle of the force vector field
+	int v_max;    ///< max speed
+
+} Path_planning;
+
 void Path_planning_update(BigStruct* all_struct);
